30f857f6f806f7be48a5a0d7fe0bac40fb922daa
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-12-19 02:37:03 +0300

    card content limited to 30 in the mvc app, we should also add this as a domain logic.

--- src/MvcApp/Models/CreateNewCardModel.cs
+++ src/MvcApp/Models/CreateNewCardModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MvcApp.Models
 {
     public class CreateNewCardModel
     {
+        [MaxLength(30)]
         public string Content { get; init; }
         
         public string BgColor { get; init; }
