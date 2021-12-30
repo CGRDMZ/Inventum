cd4f150ed43983ee9ec04ea8382fc96e4660a336
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-12-30 22:05:54 +0300

    invite user command

---
+++ src/Application/Commands/InviteUserToBoardCommand.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Application.Commands
+{
+    public class InviteUserToBoardCommand
+    {
+        public Guid BoardId { get; init; }
+        
+        
+        
+    }
+}
