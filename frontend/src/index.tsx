d247140c9176f424e837ca7d6f00cbb706ec8cfb
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2022-04-09 18:17:34 +0300

    update package json and change render with createRoot for react 18.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 
-
 import "@fontsource/caveat";
 import "@fontsource/poppins";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-
 const client = new QueryClient();
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
