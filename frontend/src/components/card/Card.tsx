ac9a6eacccd263c2bfa8c399ffedf8146b961337
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2022-04-12 20:28:51 +0300

    initial design for card detail.

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -61,20 +61,47 @@ const Card = ({
         onClose={onClose}
         scrollBehavior="inside"
       >
-        <ModalOverlay />
-        <ModalContent minH="48">
+        <ModalOverlay backdropFilter="blur(2px)" />
+        <ModalContent minH="96" bgColor={bgColor}>
           <ModalHeader>{content}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-              <Grid templateColumns="2fr 1fr" gap={1}>
-                <GridItem w="100%" minH="48">
-                    
-                </GridItem>
-                <GridItem w="100%" minH="48">
-                    <Text fontFamily="poppins" fontSize="md">Add Component</Text>
-                    <Button variant="outline"><CheckIcon /></Button>
-                </GridItem>
-              </Grid>
+            <Grid templateColumns="2fr 1fr" gap={1}>
+              <GridItem w="95%">
+                <Box minH="48" overflowY="auto">
+                  <Box bgColor="white" borderRadius="md" shadow="md" p={1}>
+                    <Text fontFamily="poppins" fontSize="sm">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Dolore provident adipisci nihil aperiam minima harum et
+                      dolorem corporis repudiandae, veniam facere sed cum
+                      impedit corrupti dolores maiores exercitationem ullam.
+                      Nihil.
+                    </Text>
+                  </Box>
+                </Box>
+              </GridItem>
+              <GridItem w="100%" minH="48">
+                <Box shadow="md" p="3" borderRadius="md" bgColor="white">
+                  <Text
+                    fontFamily="poppins"
+                    fontSize="md"
+                    fontWeight="light"
+                    fontStyle="italic"
+                  >
+                    Add Component
+                  </Text>
+                  <Button
+                    variant="solid"
+                    mr="1"
+                    mb="1"
+                    size="sm"
+                    title="Add a checklist component!"
+                  >
+                    <CheckIcon />
+                  </Button>
+                </Box>
+              </GridItem>
+            </Grid>
           </ModalBody>
         </ModalContent>
       </Modal>
